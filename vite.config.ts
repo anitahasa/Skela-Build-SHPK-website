1962751776376aa2617abe37cd9d10d40f7a3b3c
Anita Hasa <166122343+anitahasa@users.noreply.github.com>
2026-03-09 12:05:41 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: '/Skela-Build-SHPK-website/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
